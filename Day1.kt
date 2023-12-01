a84463ef72c02fa0b92d66d2b303bb7c273d2369
Saliou <saliouseck2009@gmail.com>
2023-12-01 22:59:10 +0000

    add day1 challenge of Advent of code

---
+++ Day1.kt
@@ -0,0 +1,40 @@
+import java.io.File
+val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val numbersMap = mapOf("one" to '1', "two" to '2', "three" to '3', "four" to '4', "five" to '5', "six" to '6', "seven" to '7', "eight" to '8', "nine" to '9')
+fun readFileAsLinesUsingBufferedReader(fileName: String): List<String>
+        = File(fileName).bufferedReader().readLines()
+fun main(){
+   print (secondChallenge())
+}
+private fun firstChallenge() : Int{
+    val lines = readFileAsLinesUsingBufferedReader("data.txt")
+    var sum = 0
+    lines.forEach {
+        val numberInLine = it.filter {
+            it.isDigit()
+        }
+        sum += "${numberInLine.first()}${numberInLine.last()}".toInt()
+    }
+    return sum
+}
+private fun secondChallenge() : Int{
+    val lines = readFileAsLinesUsingBufferedReader("data2.txt")
+    var sum = 0
+    lines.forEach {word->
+        var digits = mutableListOf<Char>()
+        word.forEachIndexed { index, c ->
+            if(c.isDigit()){
+                digits.add(c)
+            }else{
+                for (i in index+2..word.length){
+                    if(numbers.contains(word.substring(index,i))){
+                        numbersMap.get(word.substring(index,i))?.let { digits.add(it) }
+                    }
+                }
+            }
+        }
+        sum += "${digits.first()}${digits.last()}".toInt()
+    }
+    return sum
+}
+
